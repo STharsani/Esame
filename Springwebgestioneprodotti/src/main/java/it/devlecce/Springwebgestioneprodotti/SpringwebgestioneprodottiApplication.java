2a4dffa640674ca3d1c91cd43460d252867ed7e1
S.Tharsani <tsriskandarajah@studenti.apuliadigitalmaker.it>
2022-04-06 09:10:46 +0200

    primo commit

---
+++ Springwebgestioneprodotti/src/main/java/it/devlecce/Springwebgestioneprodotti/SpringwebgestioneprodottiApplication.java
@@ -0,0 +1,13 @@
+package it.devlecce.Springwebgestioneprodotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebgestioneprodottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebgestioneprodottiApplication.class, args);
+	}
+
+}
---
+++ Springwebgestioneprodotti/src/test/java/it/devlecce/Springwebgestioneprodotti/SpringwebgestioneprodottiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devlecce.Springwebgestioneprodotti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebgestioneprodottiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
